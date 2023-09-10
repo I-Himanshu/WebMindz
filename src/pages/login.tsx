da125bfabace9b5273f19b4527a3be7691ca132a
Anshika <anshikathakur596@gmail.com>
2023-09-10 13:49:23 +0530

    Create login.tsx

---
+++ src/pages/login.tsx
@@ -0,0 +1,12 @@
+export default function Landing() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center w-full p-24 gap-10">
+        
+          <div className="flex flex-row items-center justify-evenly w-full space-y-4">
+            <img src="https://cdn3d.iconscout.com/3d/premium/thumb/cute-robot-say-hello-5665790-4721951.png?f=webp" alt="" />
+            <div className="flex flex-col">
+            </div>
+        </div>
+    </main>
+  );
+}
