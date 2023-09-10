a1d15932ffd30ed016bc9e9c9ed2ce9ea58d9683
Anshika <anshikathakur596@gmail.com>
2023-09-10 17:16:27 +0530

    bg added

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,8 +1,8 @@
 export default function Landing() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center w-full p-24 gap-10">
+    <main className="bg flex min-h-screen flex-col items-center justify-center w-full p-24 gap-10">
         
-          <div className="flex flex-row items-center justify-evenly w-full space-y-4">
+          <div className="flex flex-row items-center justify-evenly w-full space-y-4 z-20">
             <img src="https://cdn3d.iconscout.com/3d/premium/thumb/cute-robot-say-hello-5665790-4721951.png?f=webp" alt="" />
             <div className="flex flex-col">
               <h1 className="text-[5vmin] secondaryFont">Welcome to </h1>
