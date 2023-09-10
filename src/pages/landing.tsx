e3fa55dd552d682c8f8070d816aa6b4739079e0b
Anshika <anshikathakur596@gmail.com>
2023-09-10 17:36:00 +0530

    buttons added

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -7,7 +7,11 @@ export default function Landing() {
             <div className="flex flex-col">
               <h1 className="text-[5vmin] secondaryFont">Welcome to </h1>
               <b className="text-[12vmin] -mt-6 blueGradText">WebMindz</b>
-              <p className="tertiaryFont -mt-6 mr-2 text-right font-thin opacity-70">Chat Like Never Before: AI at Your Fingertips</p>
+              <p className="tertiaryFont -mt-6 ml-5 font-thin opacity-70">Chat Like Never Before: AI at Your Fingertips</p>
+            <div className="flex flex-row justify-start mt-12 items-center">
+              <button className="grid place-items-center px-8 rounded-3xl py-3 mx-3 bg-[#4884ee] text-white secondaryFont opacity-75 transition-all hover:opacity-100">Login</button>
+              <button className="grid place-items-center px-8 rounded-3xl py-3 mx-3 bg-white text-[#4884ee] secondaryFont opacity-75 transition-all hover:opacity-100">SignUp</button>
+            </div>
             </div>
         </div>
     </main>
