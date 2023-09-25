324a621fbd8e0a1f8b145c9e83cbffa57da8131a
Anshika <anshikathakur596@gmail.com>
2023-09-25 17:53:16 +0530

    login page ui update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,21 +1,22 @@
 export default function Login() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="card flex flex-col max-w-[500px] max-h-[700px] w-full h-full bg-white p-10">
-          <h2 className="font-extrabold">Hello there</h2>
-          <p>Please enter your email & password to create an account</p>
+    <main className="flex min-h-screen flex-row items-center justify-between w-full px-40">
+        <img src="https://cdn3d.iconscout.com/3d/premium/thumb/cute-robot-say-hello-5665790-4721951.png" alt="" />
+        <div className="card flex flex-col max-w-[300px] lg:max-w-[500px] max-h-[700px] w-full h-full bg-white p-10 text-black">
+          <h2 className="font-extrabold blueGradText text-4xl">Hola!</h2>
+          <p className="mt-1">Unlock the Gateway to Your Digital Realm with WebMindz.</p>
 
-          <form className="flex flex-col mt-10 text-lg">
+          <form className="flex flex-col mt-7 text-lg">
             <div className="input-container flex-col flex">
-              <label className="font-semibold" htmlFor="email">Your email</label>
-              <input className=" border-b-2 border-cyan-400 outline-none" type="email" id="email" placeholder="Enter your email" />
+              <label className="font-semibold text-[#666] my-2" htmlFor="email">Your email</label>
+              <input className=" border-b-2 border-[#06bcfb] outline-none" type="email" id="email" placeholder="Enter your email" />
             </div> 
             <div className="input-container flex-col flex mt-5">
-              <label className="font-semibold" htmlFor="password">Your password</label>
-              <input className=" border-b-2 border-cyan-400 outline-none" type="password" id="password" placeholder="Enter your password" /> 
+              <label className="font-semibold text-[#666] my-2" htmlFor="password">Your password</label>
+              <input className=" border-b-2 border-[#06bcfb] outline-none" type="password" id="password" placeholder="Enter your password" /> 
             </div>
             
-            <button className="bg-cyan-400 text-white font-semibold mt-10 p-3 rounded-md">Continue</button>
+            <button className="text-white font-semibold mt-10 p-3 rounded-md blueGrad duration-[0.5ms] hover:scale-105">Continue</button>
           </form>
         </div>
     </main>
