8d6fefdd2ab1a4734f0c2554cc8b20cc032c0d70
Anshika <anshikathakur596@gmail.com>
2023-10-11 19:34:53 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
     <>
       {/* <Landing/> */}
       {/* <Login/> */}
-      {/* <Options/> */}
-      <UnitConverter/>
+      <Options/>
+      {/* <UnitConverter/> */}
       {/* <Games/> */}
       {/* <Chat/> */}
     </>
