cc4da4e90ab528a558796ca7264d0fbe62618e0e
Anshika <anshikathakur596@gmail.com>
2023-10-12 19:35:20 +0530

    Update speech.tsx

--- src/components/chat/speech.tsx
+++ src/components/chat/speech.tsx
@@ -1,45 +1,61 @@
 "use client";
-import { useState } from "react";
+import { useState } from 'react';
 
-export const toggleListening = (
-  listening:any,
-  setListening:any,
-  transcript:any,
-  setTranscript:any
-) => {
-    console.log("toggleListening");
-  if (window.SpeechRecognition || window.webkitSpeechRecognition) {
-
-    const recognition = new (window?.SpeechRecognition ||
-      window?.webkitSpeechRecognition)();
-    recognition.continuous = true;
-    recognition.interimResults = true;
-    console.log("recognition",recognition);
-
-    recognition.onstart = () => {
-        console.log("recognition.onstart");
-      setListening(true);
-    };
-
-    recognition.onend = () => {
-        console.log("recognition.onend");
-      setListening(false);
-    };
-
-    recognition.onresult = (event) => {
-      const interimTranscript = Array.from(event.results)
-        .map((result) => result[0].transcript)
-        .join("");
-        console.log("interimTranscript",interimTranscript);
-      setTranscript(interimTranscript);
-    };
-
-    if (listening) {
-      recognition.stop();
-    } else {
-      recognition.start();
-    }
-  } else {
-    alert("Speech recognition is not supported in your browser.");
+const SpeechToText: React.FC = () => {
+  const [transcript, setTranscript] = useState('');
+  const [listening, setListening] = useState(false);
+  interface Window {
+    SpeechRecognition: any;
   }
+  
+  const toggleListening = () => {
+    if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+      const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+      recognition.continuous = true;
+      recognition.interimResults = true;
+
+      recognition.onstart = () => {
+        setListening(true);
+      };
+
+      recognition.onend = () => {
+        setListening(false);
+      };
+
+      recognition.onresult = (event) => {
+        const interimTranscript = Array.from(event.results)
+          .map((result) => result[0].transcript)
+          .join('');
+        setTranscript(interimTranscript);
+      };
+
+      if (listening) {
+        recognition.stop();
+      } else {
+        recognition.start();
+      }
+    } else {
+      alert("Speech recognition is not supported in your browser.");
+    }
+  };
+
+  return (
+    <div className="p-4 space-y-4">
+      <h1 className="text-2xl font-semibold">Speech to Text</h1>
+      <button
+        className={`bg-${listening ? 'red' : 'green'}-600 text-white py-2 px-4 rounded`}
+        onClick={toggleListening}
+      >
+        {listening ? 'Stop Listening' : 'Start Listening'}
+      </button>
+      {transcript && (
+        <div>
+          <p className="font-semibold">Transcript:</p>
+          <p>{transcript}</p>
+        </div>
+      )}
+    </div>
+  );
 };
+
+export default SpeechToText;
