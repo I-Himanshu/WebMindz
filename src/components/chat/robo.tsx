d54a1986aef1ae3557dc89712b759a813bb24bd9
Himanshu Kumar <himanshu.kumar5403@gmail.com>
2023-10-18 00:30:28 +0530

    Character missing bug fixed

--- src/components/chat/robo.tsx
+++ src/components/chat/robo.tsx
@@ -1,26 +1,25 @@
-"use client";
-import React, { useEffect } from 'react'
-import ReactMarkdown from 'react-markdown'
-export default function Robo({msg}:any) {
-  msg = "  " + msg;
-  const [text, setText] = React.useState("");
-  
-  useEffect(() => {
-    var i = 0;
-    var speed = 1;
-    function typeWriter() {
-      if (i < msg.length) {
-        setText((prev) => prev + msg.charAt(i));
-        i++;
-        setTimeout(typeWriter, speed);
-      }
-      const scrollHere = document.getElementById("scrollHere");
-      scrollHere?.scrollIntoView({ behavior: "smooth" });
-    }
-    typeWriter();
-    return () => {};
-  }, [msg]);
+import React, { useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
 
+export default function Robo({ msg }:any) {
+  const [text, setText] = React.useState('');
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+
+  useEffect(() => {
+    const typingInterval = 10; // Adjust the typing speed as needed
+
+    if (currentIndex < msg.length) {
+      const timer = setTimeout(() => {
+        setText(msg.substring(0, currentIndex + 1));
+        setCurrentIndex(currentIndex + 1);
+      }, typingInterval);
+
+      return () => clearTimeout(timer);
+    }
+
+    const scrollHere = document.getElementById('scrollHere');
+    scrollHere?.scrollIntoView({ behavior: 'smooth' });
+  })
   return (
 
     <div className='w-[90%] flex flex-row justify-start'>
