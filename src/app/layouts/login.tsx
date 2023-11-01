7e225a82fecef836428f3461c7a6a301e352d155
Anshika <anshikathakur596@gmail.com>
2023-11-02 02:28:20 +0530

    responsiveness added

--- src/app/layouts/login.tsx
+++ src/app/layouts/login.tsx
@@ -7,14 +7,14 @@ export default function Login() {
     e.preventDefault();
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
-};
+  };
   return (
     <main className="flex min-h-screen flex-col lg:flex-row items-center justify-center lg:justify-between w-full lg:px-40 relative">
       <div className="bg1 absolute"></div>
-        <img className="hidden lg:block" src="https://cdn3d.iconscout.com/3d/premium/thumb/cute-robot-say-hello-5665790-4721951.png" alt="" />
-        <div className="cont relative translate-y-[10%] lg:translate-y-0">
+      <img className="hidden lg:block" src="https://cdn3d.iconscout.com/3d/premium/thumb/cute-robot-say-hello-5665790-4721951.png" alt="" />
+      <div className="cont relative translate-y-[10%] lg:translate-y-0">
         <img className="absolute lg:hidden top-0 left-0 -translate-y-[55%] block" src="https://cdn3d.iconscout.com/3d/premium/thumb/cute-robot-say-hello-5665790-4721951.png" alt="" />
-          <div className="card relative z-20 flex flex-col max-w-[300px] lg:max-w-[500px] max-h-[700px] w-full h-full bg-white p-10 text-black">
+        <div className="card relative z-20 flex flex-col max-w-[300px] lg:max-w-[500px] max-h-[700px] w-full h-full bg-white p-10 text-black">
           <h2 className="font-extrabold blueGradText specialFont text-4xl">Hola!</h2>
           <p className="mt-1">Unlock the Gateway to Your Digital Realm with WebMindz.</p>
 
@@ -31,13 +31,15 @@ export default function Login() {
             <button className="text-white secondaryFont font-semibold mt-10 p-3 rounded-md blueGrad duration-[0.5ms] hover:scale-105">Continue</button> */}
 
             {/* Sign In With google */}
-            <button className="mt-10 p-3 rounded-md blueGrad duration-[0.5ms] hover:scale-105" onClick={signInWithGoogle}>
-              <img className="w-6 mr-2" src="https://cdn.iconscout.com/icon/free/png-256/google-470-675827.png" alt="" />
+            <button className="mt-10 p-3 rounded-md blueGrad duration-[0.5ms] transition-all hover:scale-105 text-white text-xl flex justify-center py-6" onClick={signInWithGoogle}>
+              <div className="bg-[#ffffff8c] w-8 h-8 grid place-items-center pl-2 rounded-full mr-4 scale-60">
+                <img className="w-6 mr-2" src="https://cdn.iconscout.com/icon/free/png-256/google-470-675827.png" alt="" />
+              </div>
               Sign In With Google
             </button>
           </form>
         </div>
-        </div>
+      </div>
     </main>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -13,19 +13,19 @@ export default function ProfilePage() {
 
     return <>
         <div className='w-full min-h-screen flex flex-col items-center justify-center'>
-            <div className="w-[700px] h-[250px] bg-[#67676734] flex relative shadow-lg shadow-[#ffffff1d]">
+            <div className="w-[300px] md:w-[700px] min-h-[250px] bg-[#67676734] flex flex-col md:flex-row relative shadow-lg pb-12">
                 <div className="absolute top-[-20px] right-[10px] -skew-x-6 px-8 py-2 blueGrad">
                     <p>Profile</p>
                 </div>
-                <div className="w-[40%] h-full">
+                <div className="w-full md:w-[40%] h-full">
                     <img className="w-full h-full" src={user?.photoURL} alt={user?.displayName} />
                 </div>
-                <div className="relative flex flex-col px-8 pt-5">
-                    <div className="font-bold text-[100px] mb-2 primaryFont opacity-60">{user?.displayName}</div>
-                    <p className="text-xl mt-[-40px] font-thin opacity-30">
+                <div className="flex flex-col px-8 pt-5">
+                    <div className="font-bold text-[45px] md:text-[100px] mb-2 primaryFont opacity-60 leading-[55px] md:leading-[110px]">{user?.displayName}</div>
+                    <p className="text-sm md:text-xl md:mt-[-10px] mb-[30px] md:mb-0 font-thin opacity-30">
                         {user?.email}
                     </p>
-                    <p className="absolute bottom-4 right-4 rounded-full py-1 text-sm font-semibold text-gray-700 mr-2 specialFont blueGradText opacity-50">{user?.uid}</p>
+                    <p className="absolute bottom-1 right-1 md:bottom-4 md:right-4 rounded-full py-1 text-sm font-semibold text-gray-700 mr-2 specialFont blueGradText opacity-50">{user?.uid}</p>
                 </div>
             </div>
         </div>
